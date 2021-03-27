7e113ccff5b4052d0f983b974633c1796976d061
monaliisa <mmss@cin.ufpe.br>
2021-03-27 13:20:02 -0300

    innitial commit

---
+++ ms-programmer/src/main/java/com/sousa/monalisa/msprogrammer/MsProgrammerApplication.java
@@ -0,0 +1,13 @@
+package com.sousa.monalisa.msprogrammer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsProgrammerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsProgrammerApplication.class, args);
+	}
+
+}
